7c954e16a399fcfd4cc0ca3f723e60d6e0172db4
Lemuel Catacutan <lemueljohncatacutan@gmail.com>
2026-03-01 14:55:30 +0800

    Add files via upload

---
+++ Catacutan/Catacutan_AreaCircle.py
@@ -0,0 +1,12 @@
+import math
+
+print("Lemuel John S. Catacutan | BSIS 3AG2")
+print("Area of the Circle")
+print("=========================")
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = math.pi * (radius ** 2)
+
+print(f'The area of the circle is: {area:.2f}')
+print("=========================")
