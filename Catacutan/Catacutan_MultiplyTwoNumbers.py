6993aa830a2b3c8bfecb8d01943aa7673c8a7a5a
Lemuel Catacutan <lemueljohncatacutan@gmail.com>
2026-03-01 14:48:02 +0800

    Add files via upload

---
+++ Catacutan/Catacutan_MultiplyTwoNumbers.py
@@ -0,0 +1,8 @@
+print("Lemuel John S. Catacutan | BSIS 3AG2")
+print("Multiply Two Numbers")
+print("=========================")
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+print("Result: ", x, " x ", y, " = ", x * y)
